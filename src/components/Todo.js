2b3dc024d24e8cb4fc0282c8519d3626d2ecdd8c
Datta <2096688@cognizant.com>
2022-05-09 14:30:25 +0530

    added function on click of delete button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,15 @@
 
 function Todo(props) {
+    const clickHandler = () =>{
+        console.log(props.title + ' Got Clicked')
+    }
   return (
     <div>
       <h1>My Todos</h1>
       <div className='card'>
         <h2>{props.title}</h2>
         <div className='actions'>
-          <button className='btn'>Delete</button>
+          <button className='btn' onClick={clickHandler}>Delete</button>
         </div>
       </div>
     </div>
