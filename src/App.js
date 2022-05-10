04b3ab4d1db61f53f4592bdeec7ee2523726eff3
Datta <2096688@cognizant.com>
2022-05-10 15:23:29 +0530

    final cleanup for meetup application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import Todo from "./components/Todo";
-
 function App() {
-  return (
-    <div className="App">
-      <Todo title="Todo 1"></Todo>
-      <Todo title="Todo 2"></Todo>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/components/Backdrop.js
+++
@@ -1,6 +0,0 @@
-const Backdrop = (props) => {
-
-  return <div className="backdrop" onClick={props.onBackdropClick}></div>;
-};
-
-export default Backdrop;
